11e8d5ccda3784ff7d3309367d7218e53b8fc9f9
zqyu <zqyu@streamax.com>
2020-11-03 10:55:35 +0800

    fix

---
+++ src/page/testVirtualTable/index.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import VirtualTable from '../../components/virtualTable'
+function testVirtualTable () {
+  const columns = [
+    {
+      title: 'A',
+      dataIndex: 'key',
+      // width: 150,
+    },
+    {
+      title: 'B',
+      dataIndex: 'key',
+    },
+    {
+      title: 'C',
+      dataIndex: 'key',
+    },
+    {
+      title: 'D',
+      dataIndex: 'key',
+    },
+    {
+      title: 'E',
+      dataIndex: 'key',
+      // width: 200,
+    },
+    {
+      title: 'F',
+      dataIndex: 'key',
+      // width: 100,
+    },
+  ];
+  const data = Array.from(
+    {
+      length: 1000,
+    },
+    (_, key) => ({
+      key,
+    }),
+  );
+  return (
+    <VirtualTable
+      columns={columns}
+      dataSource={data}
+      scroll={{
+        y: 800,
+        x: '100vw',
+      }}
+    />
+  )
+}
+
+export default testVirtualTable
